8aff9fc4fe6596e035776e3608bf8e74580341ef
Luke Mcfarlane <lmcfarlane97@hotmail.com>
2023-01-22 16:17:52 +0000

    Added grid size selector and improved layout

--- script.js
+++ script.js
@@ -23,19 +23,33 @@ btn.addEventListener('click', resetFunction);
 
 
 function resetFunction() {
-    for (let i = 0; i < 256; i++) {
-        const blocks = document.querySelector('.section');
-        gameArea.removeChild(blocks)
+    let s = document.getElementById("switcher");
+    let switchPos = s.value;    
+     while (gameArea.firstChild) {
+        gameArea.removeChild(gameArea.lastChild);
     }
-    for (let i = 0; i < 256; i++) {
-        const square = document.createElement('div');               
+    let cols = Math.sqrt(switchPos);
+    gameArea.style.gridTemplateColumns = "repeat(" + cols + ", 1fr)"
+    for (let i = 0; i < switchPos; i++) {
+        const square = document.createElement('div');   
         square.classList.add('section');    
         square.setAttribute('id', i);              
-        gameArea.appendChild(square);  
+        gameArea.appendChild(square);       
 }
 const pixels = document.querySelectorAll('.section');
 
 pixels.forEach((pixel) => {
+    if (cols === 16) {
+        pixel.style.width = "35px";
+        pixel.style.height = "35px";
+    } else if (cols === 32) {
+        pixel.style.width = "17px";
+        pixel.style.height = "17px";
+    } else if (cols === 48) {
+        pixel.style.width = "11px";
+        pixel.style.height = "11px";
+    }
+
     pixel.addEventListener('click', () => {
         pixel.style.backgroundColor = 'black';
     });
