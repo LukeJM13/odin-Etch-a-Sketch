1e00bab5659e3083b9602f06006d49d09ed910d5
Luke Mcfarlane <lmcfarlane97@hotmail.com>
2023-01-22 14:48:45 +0000

    Added clickable squares and script for reset button

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const gameArea = document.querySelector('.mainBody');
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');               
     square.classList.add('section');    
-    square.setAttribute('id', i);                                
+    square.setAttribute('id', i);   
     gameArea.appendChild(square);    
 }
+
+const pixels = document.querySelectorAll('.section');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('click', () => {
+        pixel.style.backgroundColor = 'black';
+    });
+  });
+
+
 
 btn.addEventListener('click', resetFunction);
 
@@ -20,7 +30,15 @@ function resetFunction() {
     for (let i = 0; i < 256; i++) {
         const square = document.createElement('div');               
         square.classList.add('section');    
-        square.setAttribute('id', i);                                  
+        square.setAttribute('id', i);              
         gameArea.appendChild(square);  
 }
+const pixels = document.querySelectorAll('.section');
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('click', () => {
+        pixel.style.backgroundColor = 'black';
+    });
+  });
 }
+
